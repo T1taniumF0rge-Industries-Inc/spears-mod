ca99cceb2280b2f95139b7c84c44cb10edd70b09
Titan1um <gamersoftware.corp@gmail.com>
2026-03-02 22:05:27 +0100

    Fix loader metadata and entrypoints across Forge/Fabric/NeoForge/Quilt

--- forge/src/main/java/com/titan1um/spears/forge/ForgeSpearsEntrypoint.java
+++ forge/src/main/java/com/titan1um/spears/forge/ForgeSpearsEntrypoint.java
@@ -1,7 +1,9 @@
 package com.titan1um.spears.forge;
 
 import com.titan1um.spears.common.SpearRegistry;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod("spearsmod")
 public final class ForgeSpearsEntrypoint {
     public ForgeSpearsEntrypoint() {
         SpearRegistry.spearIds();
--- quilt/src/main/java/com/titan1um/spears/quilt/QuiltSpearsEntrypoint.java
+++ quilt/src/main/java/com/titan1um/spears/quilt/QuiltSpearsEntrypoint.java
@@ -1,8 +1,10 @@
 package com.titan1um.spears.quilt;
 
 import com.titan1um.spears.common.SpearRegistry;
+import net.fabricmc.api.ModInitializer;
 
-public final class QuiltSpearsEntrypoint {
+public final class QuiltSpearsEntrypoint implements ModInitializer {
+    @Override
     public void onInitialize() {
         SpearRegistry.spearIds();
     }
